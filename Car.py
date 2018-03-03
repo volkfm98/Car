3f7bd95f39ce1c4220e81c32146157f730d8c01c
Shiksx <Shiksx@yandex.ru>
2018-03-03 16:32:57 +0500

    Update Car.py

--- Car.py
+++ Car.py
@@ -10,12 +10,12 @@ class Car: #три основных метода которые будут ис�
         self.CarCon=CarControl(device) # кар контролу передаем девайс которым пользуемся
         self.TroubleDet=self.TroubleChecking(video_source)
         self.LineDet=self.LineChecking(video_source)
+        self.WallDet=self.WallThread() #детектор стен
 
     class TroubleChecking(Thread):  #поток для детектирования знаков и ситуаций на дороге
         def __init__(self,video_source):
             Thread.__init__(self)
             self.Detector=Detector('signs/model.yaml','signs/model.h5','semafor/model.yaml','semafor/model.h5',True,video_source)
-            self.crossroad=0
             self.signs = 0
             self.RedIsON=False
             self.mark=False
@@ -28,7 +28,6 @@ class Car: #три основных метода которые будут ис�
                     self.signs = self.Detector.detectSigns(frame, self.Detector.printFlag)
                     self.RedIsON = self.Detector.detectSemafors(frame,self.Detector.printFlag) #1 - кирпич (красный знак стоп) 3 - движение вперед 4 - направо 5 - налево 6 - прямо или направо 7 - прямо или налево
                     frame = self.Detector.cap.read()[1]
-                    self.crossroad = self.Detector.DetectCrossroads()  # так этого нет но оно бы пригодилось
 
             self.Detector.cap.release()
 
@@ -40,17 +39,41 @@ class Car: #три основных метода которые будут ис�
             self.cap = cv2.VideoCapture(video_source)
             self.lines = 0
             self.mark=False
+            self.parking=False
+
+        def run(self):
+            
+            self.mark=True
+            if (not self.parking):
+                frame = self.cap.read()[1]
+                vecs = [[-3, -1, 70], [3, -1, 70]]
+                self.Road = LineDetector.RoadControl(frame, 240, vecs, viz=True)
+
+                while (self.mark):
+                    self.Detector.line_detector.img = frame
+                    print(self.Road.poke())
+                    self.lines = self.Road.poke()
+            else:
+                while (self.mark):
+                    #detect parking
+                
+                
+                
+              
+    class WallThread(Thread):  #поток для детектирования стен
+        def __init__(self,device):
+            Thread.__init__(self)
+            self.cap = cv2.VideoCapture(video_source)
+            self.walls = 0 #0 слева 1 спереди 2 справа
+            self.mark=False
+            self.WD=WallDetector()
 
         def run(self):
             self.mark=True
-            frame = self.cap.read()[1]
-            vecs = [[-3, -1, 70], [3, -1, 70]]
-            self.Road = LineDetector.RoadControl(frame, 240, vecs, viz=True)
+            self.walls = self.WD.Detect()
 
             while (self.mark):
-                self.Detector.line_detector.img = frame
-                print(self.Road.poke())
-                self.lines = self.Road.poke()
+                self.walls = self.WD.Detect()
 
 
 
@@ -66,16 +89,22 @@ class Car: #три основных метода которые будут ис�
     '''разобраться со знаками'''
     def SignHandler(self,sign): #метод обработки знаков
         if (sign==1):#кирпич
+            
             pass
         elif (sign==3):#движение вперед
+            
             pass
         elif (sign == 4):#направо
+            
             pass
         elif (sign == 5):#налево
+            
             pass
         elif (sign == 6):#прямо или направо
+            
             pass
         elif (sign == 7):#прямо или налево
+            
             pass
         else:
             pass
@@ -116,10 +145,13 @@ class Car: #три основных метода которые будут ис�
             if (self.TroubleDet.signs!=0):
                 self.SignHandler(self.TroubleDet.signs)
             else:
-                if self.LineDet.lines==2: #надо поменять
+                if self.LineDet.lines[0] or self.walls[0]: #отъезжаем от стены или от линии подобрать константы
+                    self.CarCon.move()
+                    
                     pass
                     #отворачиваем
-                if self.LineDet.lines == 1:  # надо поменять
+                if self.LineDet.lines[1] or self.walls[2]:  # 
+                    self.CarCon.move()
                     pass
                     #отворачиваем
                 else:
@@ -127,16 +159,12 @@ class Car: #три основных метода которые будут ис�
 
         return self.TroubleDet.crossroad #иначе завершаем движение и выдаем почему завершили
 
-    '''тут надо павла просить'''
-    def LookAround(self):  #детект парковки
-
-
-
-        return
 
 
     '''почти все отлично но перекрестки!!'''
     def MainRoad(self):# просто едем по по линии и поворачиваем на первых? поворотах направо
+        
+        self.WallDet.start()
         self.TroubleDet.start()
         self.LineDet.start()
         for i in range(2):#всего два поворота ведь так7
@@ -146,11 +174,13 @@ class Car: #три основных метода которые будут ис�
                 self.TurnOn(1)
         self.TroubleDet.mark = False
         self.LineDet.mark = False
+        self.WallDet.mark= False
         return 1  # по идее должна вернуть значение обозначающее на каком повороте мы заехали
 
 
     '''детектор знаков сюда встроить'''
     def CityRoad(self):
+        self.WallDet.start()
         self.TroubleDet.start()
         self.LineDet.start()
         self.map = Map.MyMap(open('newmap.txt')) #нам нужна карта для построения маршрута
@@ -160,24 +190,24 @@ class Car: #три основных метода которые будут ис�
             if dot.id==18:
                 finishDot=dot
         self.map.SetDirections(open('newdirections.txt'))
-        Path=self.map.FindTheWay(startDot,finishDot) #теперь наш путь лежит в path
+        self.Path=self.map.FindTheWay(startDot,finishDot) #теперь наш путь лежит в path
         prev=str(-2) #-2 если на втором повороте заехали, -1 если на первом для клучей
         #идея такая пытаемся поехать в нужном направлении не получилось удаляем ребро, по новой считаем
         while (startDot!=finishDot): #пока не доехали до финиша
-            for joint in Path:
+            for joint in self.Path:
                 direction=self.map.directions[prev+str(joint.id)] #смотрим направление поворота на данном перекресте
                 self.Car.TurnOn(direction) #поворачиваем на повороте 0 прямо 1 право -1 влево 2 круговое движение
-                trouble=self.StayOnTheLine() #едем и держимся линии пока ничего не мешает
+                sign=self.StayOnTheLine() #едем и держимся линии пока ничего не мешает
                 prev=str(joint.id) #для вычисления следующего направления запоминаем ребро по которому поехали
                 if (self.TroubleDet.signs!=1): #если что-то помешало придется вернутся и перестроить маршрут удаляя это ребро
                     #аналогично if(trouble==3) можно использовать
                     joint.Delete()
-                    Path=self.map.FindTheWay(startDot,finishDot)
+                    self.Path=self.map.FindTheWay(startDot,finishDot)
                     self.TurnOn(-2) #придется развернутся -2 разворот
                     break
             else:
                 startDot=joint.GetNegative(startDot)#если оказались на перекрестке продолжаем движение'''
-
+        self.WallDet.mark=False
         self.TroubleDet.mark = False
         self.LineDet.mark = False
         return 2
@@ -185,23 +215,30 @@ class Car: #три основных метода которые будут ис�
 
     '''ага'''
     def CircleRoad(self):
-
-
-
-
-
-
-
-
+        
+        
+        
+        While (not DetectedCrossroad):
+            self.StayOnTheLine()
+        
         return 3
 
 
 
     '''тут все доделать'''
     def Parking(self):
+        self.WallDet.start()
+        self.LineDet.parking=True
         self.TroubleDet.start()
         self.LineDet.start()
-        self.LookAround()#надо как-то осмотреться
+        
+        While (ParkingDis меньше const): #подъезжаем
+           self.CarCon.move()
+        
+        #паркуемся
+        
+        
+        self.WallDet.mark=False
         self.TroubleDet.mark = False
         self.LineDet.mark = False
         return 4
